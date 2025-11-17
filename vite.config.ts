f25c362273ad6fe6cd02a9b7c040a310fcc7229f
Boudrigakhaoula <khaoulaboudriga@gmail.com>
2025-11-17 17:22:20 +0100

    update vite.config & package

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // When building for GitHub Pages under a repo (not username.github.io),
-  // set `base` to the repo name so built asset paths point to `/portfolio/`.
-  base: mode === "development" ? "/" : "/portfolio/",
+  base: "/portfolio", 
   server: {
     host: "::",
     port: 8080,
